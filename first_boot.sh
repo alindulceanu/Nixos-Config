21ba49cc1df4a1cbd88e6ca6e91ea68fe54e6717
Alin <alin.dulceanu@gmail.com>
2025-07-09 19:38:56 +0300

    first boot improve

--- first_boot.sh
+++ first_boot.sh
@@ -9,10 +9,6 @@ cat ~/.ssh/id_ed25519.pub
 echo "Please add this key to github account"
 read -n 1 -s -r -p "Press any key to continue..."
 
-rm -rf ~/.dotfiles
-git clone git@github.com:alindulceanu/Nixos-Config.git ~/.dotfiles && cd ~/.dotfiles
-git apply ~/user-patch.patch
-sudo cp /etc/nixos/hardware-configuration.nix .
 home-manager switch --flake .
 
 
