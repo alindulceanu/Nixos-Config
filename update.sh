c7e23ea22b8df287e68af625a18d8380ca4a25ba
Alin <alin.dulceanu@gmail.com>
2025-07-09 18:08:59 +0300

    update.sh updated

--- update.sh
+++ update.sh
@@ -4,7 +4,11 @@ echo "Did you pushed all of your changes? (yes/no)"
 read -r CONFIRM
 
 if [ $CONFIRM == "yes" ]; then
+  git restore --staged .
   git restore .
+  mv hardware-configuration.nix ../
   git pull
+  mv ../hardware-configuration.nix .
+  git add hardware-configuration.nix
   git apply ~/user-patch.patch
 fi
