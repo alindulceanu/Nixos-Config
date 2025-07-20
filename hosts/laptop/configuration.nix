5ea6d08731aab1c416444929fd5fc6e70b284eae
Alin <alin.dulceanu@gmail.com>
2025-07-20 19:28:53 +0300

    Added laptop setup

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,124 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ lib, pkgs-stable, pkgs-unstable, config, ... }:
+let
+  systemSettings = {
+    timezone = "Europe/Bucharest";
+    profile = "personal"; # todo
+    locale = "en_US.UTF-8";
+    bootMode = "uefi"; # todo
+    gpuType = "intel"; # todo
+    dm = "sddm"; # todo
+    displayServer = "wayland";
+    username = "aln";
+    hostname = "laptop";
+  };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./../../system
+    ];
+  config = {
+    # System software settings
+    flatpak.enable = true;
+    virt.enable = true;
+    bluetooth.enable = true;
+    gpuSoft.enable = true; 
+    automount.enable = true;
+    firewall.enable = true;
+    ssh.enable = false;
+    spice.enable = false;
+    bootScreen.theme = "cubes";
+#    audio = {
+#      enable = true;
+#      backend = "pulseaudio";
+#    };
+
+    settings = {
+      timezone = "Europe/Bucharest";
+      locale = "en_US.UTF-8";
+    };
+    
+    swapDevices = [{
+      device = "/.swapfile";
+      size = 8192;
+    }];
+
+    networking = {
+      enable = true;
+      proxyUrl = null;
+    };
+
+    services.dbus.enable = true;
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    programs.hyprland.enable = true;
+
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    services.xserver.enable = true;
+
+    services.displayManager.sddm = {
+      enable = true;
+      theme = "chili";
+      wayland.enable = true;
+    };
+
+    systemd.services.display-manager = {
+      after = [ "systemd-udev-settle.service" ];
+      wants = [ "system-udev-settle.service" ];
+    };
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+      layout = "ro";
+      variant = "";
+    };
+    # Enable touchpad support (enabled default in most desktopManager).
+    services.libinput.enable = true;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.${systemSettings.username} = {
+      isNormalUser = true;
+      description = "${systemSettings.username}";
+      extraGroups = [ "libvirtd" "networkmanager" "wheel" "audio" "video" ];
+      shell = pkgs-stable.zsh;
+    };
+
+    programs.zsh.enable = true;
+
+    security.sudo.enable = true;
+    security.sudo.wheelNeedsPassword = true;
+
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs-stable; [
+      wget
+      git
+      tree
+      zsh
+      sddm-chili-theme
+    ] ++ ( with pkgs-unstable; [
+
+      ]
+    );
+
+    system.stateVersion = "25.05";
+
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+  };
+
+  options = {
+    systemSettings.hostname = lib.mkOption {
+      type = lib.types.str;
+      default = "laptop";
+      description = "Sets the machine's Host Name";
+    };
+  };
+}
---
+++ hosts/laptop/home.nix
@@ -0,0 +1,125 @@
+{ config, pkgs, pkgs-unstable, ... }:
+{
+  imports = [
+    ./../../user
+  ];
+
+  gitSettings = {
+    name = "alin";
+    email = "alin.dulceanu@gmail.com";
+  };
+
+  # Stylix
+  style = {
+    theme = "space1";
+    font = {
+      name = "FiraCode Nerd Font";
+      package = pkgs.nerd-fonts.fira-code;
+    };
+    cursor = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+    };
+  };
+
+  stylix.enableReleaseChecks = false; 
+  waybar.enable = true;
+  hypr.enable = true;
+  terminals.kitty.enable = true;
+
+  home.username = "alin";
+  home.homeDirectory = "/home/alin";
+
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [
+    fastfetch
+    syncthing
+    libreoffice
+    kdePackages.okular
+    zsh
+    gimp
+    spotify
+    discord
+    gfn-electron
+    teams-for-linux
+    adwaita-icon-theme
+    obs-studio
+    devbox
+    zapzap
+    vlc
+    wineWowPackages.stable
+    winetricks
+    keepassxc
+    keepmenu
+    p7zip
+    fd
+    pywal
+  ] ++ ( with pkgs-unstable; [
+#    wineWowPackages.waylandFull
+  ]);
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    TERM = "kitty";
+    BROWSER = "firefox";
+  };
+
+  xdg.enable = true;
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    music = "${config.home.homeDirectory}/Media/Music";
+    videos = "${config.home.homeDirectory}/Media/Videos";
+    pictures = "${config.home.homeDirectory}/Media/Pictures";
+    download = "${config.home.homeDirectory}/Downloads";
+    documents = "${config.home.homeDirectory}/Documents";
+
+    desktop = null;
+    publicShare = null;
+
+    extraConfig = {
+      XDG_DOTFILES_DIR = "${config.home.homeDirectory}/.dotfiles";
+      XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
+      XDG_VM_DIR = "${config.home.homeDirectory}/Machines";
+    };
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+
+    associations.added = {
+      "text/plain" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "application/pdf" = [ "org.kde.okular.desktop" "firefox.desktop" ];
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "writer.desktop" ];
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "calc.desktop" ];
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [ "impress.desktop" ];
+    };
+
+    defaultApplications = {
+      "text/plain" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "video/mp4" = [ "vlc.desktop" ];
+      "video/x-matroska" = [ "vlc.desktop" ];
+      "video/webm" = [ "vlc.desktop" ];
+      "video/x-msvideo" = [ "vlc.desktop" ];
+      "video/x-ms-wmv" = [ "vlc.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "application/pdf" = [ "firefox.desktop" ]; 
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = [ "writer.desktop" ];
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "calc.desktop" ];
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation" = [ "impress.desktop" ];
+    };
+  };
+
+  gtk.iconTheme = {
+    package = pkgs.papirus-icon-theme;
+    name = if (config.stylix.polarity == "dark") then "Papirus-Dark" else "Papirus-Light";
+  };
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
