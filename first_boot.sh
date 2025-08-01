e8e9bab5e8510bd0d2e94a2e1457fb82ac6a7c2d
alin <alin.dulceanu@gmail.com>
2025-08-02 00:23:22 +0300

    hotfix install.sh

--- first_boot.sh
+++ first_boot.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-NAME=whoami
+NAME=$(whoami)
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
+sudo -u "$NAME" nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+sudo -u "$NAME" nix-channel --update
+sudo -u "$NAME" nix-shell '<home-manager>' -A install
 cd .. && sudo chown -R "$NAME" .dotfiles/
 cd .dotfiles || exit
 
-home-manager switch --flake .
+sudo -u "$NAME" home-manager switch --flake .
 
 
